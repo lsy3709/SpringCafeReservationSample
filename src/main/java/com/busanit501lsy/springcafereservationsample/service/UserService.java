e5c694457cd0d3c95ab622c925e62719b09b17ac
이상용 <lsy3709@naver.com>
2024-08-29 17:37:40 +0900

    패스워드 인코딩 2번 수정, 헤더에 프로필 아이디 예외처리 수정 필요함.

--- src/main/java/com/busanit501lsy/springcafereservationsample/service/UserService.java
+++ src/main/java/com/busanit501lsy/springcafereservationsample/service/UserService.java
@@ -50,7 +50,7 @@ public class UserService {
 
     public User createUser(User user) {
         user.addRole(MemberRole.USER);
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
