3ec9fa467b68dbf1e726d26d2c378d2ed98ba155
이상용 <lsy3709@naver.com>
2024-08-29 17:52:25 +0900

    엔티티 @ToString 주석

--- src/main/java/com/busanit501lsy/springcafereservationsample/entity/Reservation.java
+++ src/main/java/com/busanit501lsy/springcafereservationsample/entity/Reservation.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@ToString
+//@ToString
 @Getter
 @Setter
 @AllArgsConstructor
--- src/main/java/com/busanit501lsy/springcafereservationsample/entity/User.java
+++ src/main/java/com/busanit501lsy/springcafereservationsample/entity/User.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
-@ToString
+//@ToString
 @Getter
 @Setter
 @AllArgsConstructor
