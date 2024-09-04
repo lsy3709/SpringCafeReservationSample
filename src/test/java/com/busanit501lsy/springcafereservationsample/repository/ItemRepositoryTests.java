f3490c0465b9cfa19e4453bf9f8276f3b3a07290
lsy3709 <lsy3709@naver.com>
2024-09-05 08:09:44 +0900

    mvc, 상품 목록 출력, 반응형으로 , 카드뷰롤 3_3, 2_2,1_1로 기본 화면 구조 구성,

--- src/test/java/com/busanit501lsy/springcafereservationsample/repository/ItemRepositoryTests.java
+++ src/test/java/com/busanit501lsy/springcafereservationsample/repository/ItemRepositoryTests.java
@@ -1,12 +1,10 @@
 package com.busanit501lsy.springcafereservationsample.repository;
 
 import com.busanit501lsy.springcafereservationsample.entity.Item;
-import com.busanit501lsy.springcafereservationsample.entity.User;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
 import java.util.stream.IntStream;
@@ -25,7 +23,7 @@ public class ItemRepositoryTests {
                     .name("상품"+i)
                     .price(BigDecimal.valueOf(100+i))
                     // 아래 이미지는 현재 몽고디비에 있는 임의의 이미지를 재사용함
-                    .itemRepImageId("66d7cc29e1fc5d445d8920b7")
+                    .itemRepImageId("66d8c42f2a112c0ea296df33")
                     .description("상품 설명 " + i)
                     .build();
 
